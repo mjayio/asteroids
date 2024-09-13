e77f5300e02224389338c211a9c78dd7d22b7974
mjayio <mjdeveng@gmail.com>
2024-09-12 21:46:13 -0400

    Create player triangle on screen

--- main.py
+++ main.py
@@ -3,6 +3,7 @@
 # throughout this file
 import pygame
 from constants import *
+from player import *
 
 def main():
     pygame.init()
@@ -10,11 +11,19 @@ def main():
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    x = SCREEN_WIDTH / 2
+    y = SCREEN_HEIGHT / 2
+    pl = Player(x, y)
+    pl.draw(screen)
+    time = pygame.time.Clock()
+    dt = 0
     while(True):
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         pygame.display.flip()
+        dt = (time.tick(60)) / 1000
+        
     
 
 if __name__ == "__main__":
---
+++ player.py
@@ -0,0 +1,31 @@
+import circleshape as cshape
+import constants as cVals
+import pygame
+
+class Player(cshape.CircleShape):
+    def __init__(self, x, y):
+        super().__init__(x, y, cVals.PLAYER_RADIUS)
+        self.rotation = 0
+        
+    # in the player class
+    def triangle(self):
+        forward = pygame.Vector2(0, 1).rotate(self.rotation)
+        right = pygame.Vector2(0, 1).rotate(self.rotation + 90) * self.radius / 1.5
+        a = self.position + forward * self.radius
+        b = self.position - forward * self.radius - right
+        c = self.position - forward * self.radius + right
+        return [a, b, c]
+
+    def draw(self, screen):
+        # super().draw(screen)
+        pygame.draw.polygon(screen, "white", self.triangle(), width=2)
+        
+    def rotate(self, dt):
+        keys = pygame.key.get_pressed()
+
+        if keys[pygame.K_a]:
+            # ?
+        if keys[pygame.K_d]:
+            # ?
+        
+    
